4d78200a4b59496e7e67751b5115075eac036b34
StupidWind <pex2460@163.com>
2019-09-17 22:05:23 +0800

    修改部分的文章接口

--- src/apis/article-request.js
+++ src/apis/article-request.js
@@ -1,5 +1,6 @@
 import axios from '@/libs/api.request.js';
 
+// 分页获取文章列表
 export const getArticleList = params => {
     return axios.request({
         params: params,
@@ -8,9 +9,18 @@ export const getArticleList = params => {
     });
 };
 
+// 获取文章详情信息
 export const getArticleDetail = id => {
     return axios.request({
         url: '/article/get/' + id,
         method: 'get'
+    });
+};
+
+// 删除文章
+export const delArticle = id => {
+    return axios.request({
+        url: '/article/del/' + id,
+        method: 'delete'
     });
 };
